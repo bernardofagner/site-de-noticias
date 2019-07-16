66e9900a0186d534cbf71269e8bbafd5e6147356
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2019-07-16 19:13:16 -0300

    Criação dos cards ds pagina inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+//App copmponents
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
 
 // Não pode colocar '/' no path, senão da erro
 const routes: Routes = [
